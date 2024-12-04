7dc92aa2803c0f5ef4a8e2592892594321f4feba
codeE99 <csherman3102@gmail.com>
2024-12-04 03:31:07 -0500

    Update GameServer.py
    Added core game itself, object speed increase, restart function, and score.

--- GameServer.py
+++ GameServer.py
@@ -2,52 +2,119 @@ import threading
 import pygame
 import socket
 import sys
+import random
 
 name = "test"
 posx = 300
-posy = 200
+posy = 350
+score=0
+screen_size = screen_width, screen_height = 600, 400
+ball_width=30
+ball_height=30
+ball_speed=1
+ball_x=random.randint(0,screen_width-ball_width)
+ball_y=ball_height
+gameover=False
 
 def GameThread():
-    pygame.init()
+    pygame.init() #initializes the game
+
+    #colors of the background and game
     background = (204, 230, 255)
-    shapeColor = (0, 51, 204)
-    shapeColorOver = (255, 0, 204)
-    
+    textColor = (0, 0, 0)
+    bucketColor= (128, 128, 128)
+    ballColor=(255,0,0)
+
+
     fps = pygame.time.Clock()
-    screen_size = screen_width, screen_height = 600, 400
-    rect2 = pygame.Rect(0, 0, 75, 75)
-    rect1 = pygame.Rect(0, 0, 25, 25)
+    global screen_size
+
+    rect1 = pygame.Rect(0, 0, 40, 40)
+    
+    #creating our ball
+    global ball_width
+    global ball_height
+    global ball_speed
+
+    def draw_ball(x,y):
+        pygame.draw.ellipse(screen, ballColor, [x,y,ball_width, ball_height])
+    
+    def display_score(score):
+        font=pygame.font.SysFont(None, 36)
+        text=font.render("Score: "+str(score), True, textColor)
+        screen.blit (text, (10,10)) #adds image onto screen
+
+    def game_over():
+        global gameover
+        font=pygame.font.SysFont(None, 72)
+        text=font.render("GAME OVER", True, textColor)
+        screen.blit(text,(screen_width//2-150,screen_height//2-36))
+        display_restart=font.render("Press R to restart", True, textColor)
+        screen.blit(display_restart,(screen_width//3-200,screen_height//3-40))
+        gameover=True
+
+    #screen size
     screen = pygame.display.set_mode(screen_size)
     pygame.display.set_caption('Welcome to CCN games')
-    
-    colorRect = (shapeColor)
-    colorRect2 = (shapeColorOver)
+   
+    colorRect = (bucketColor) 
     global posx 
     global posy 
-    while True:
+    global ball_x
+    global ball_y 
+    global score
+    while True: 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:    
                 pygame.quit()
                 sys.exit()
-
+        
         screen.fill(background)
+
         rect1.center = (posx, posy)
-        collision = rect1.colliderect(rect2)
+
+        if posx < 0:
+            posx=0
+        elif posx>screen_width-rect1.width:
+            posx=screen_width-rect1.width
+        if posy<0:
+            posy=0
+        elif posy>screen_height-rect1.height:
+            posy=screen_height-rect1.height
+
+        ball_y += ball_speed
+
+        if ball_y+ball_height>posy and posx<ball_x+ball_width<posx+rect1.width:
+            score+=1
+            ball_speed+=0.5
+            ball_x=random.randint(0,screen_width-ball_width)
+            ball_y=-ball_height
+            
+        draw_ball(ball_x,ball_y)
+        display_score(score)
+
         pygame.draw.rect(screen, colorRect, rect1)
-        if collision:
-            pygame.draw.rect(screen, colorRect2, rect2, 6, 1)
-        else:
-            pygame.draw.rect(screen, colorRect, rect2, 6, 1)
+        draw_ball(ball_x,ball_y)
+
+        if ball_y>screen_height:
+            game_over()
+
         pygame.display.update()
         fps.tick(60)
 
-
-    pygame.quit()
+   # pygame.quit()
 
 
 def ServerThread():
     global posy
     global posx
+    global score
+    global ball_width
+    global ball_height
+    global ball_speed
+    global ball_x
+    global ball_y
+    global gameover
     # get the hostname
     host = socket.gethostbyname(socket.gethostname())
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -74,13 +141,24 @@ def ServerThread():
         
         print("from connected user: " + str(data))
         if(data == 'w'):
-            posy -= 10
+            posy -= 25             
         if(data == 's'):
-            posy += 10
+            posy += 25              
         if(data == 'a'):
-            posx -= 10
+            posx -= 25           
         if(data == 'd'):
-            posx += 10
+            posx += 25  
+        if(data == 'r' and gameover):   
+            posx = 300
+            posy = 200
+            score=0
+            ball_width=30
+            ball_height=30
+            ball_speed=1
+            ball_x=random.randint(0,screen_width-ball_width)
+            ball_y=ball_height
+            gameover=False
+            print('Game reset')
     conn.close()  # close the connection
 
 
